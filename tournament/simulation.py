0afb71d23227d950357f0138c64384f04c4fcb02
lorenzosc <lorenzocorrea6@hotmail.com>
2024-04-09 01:16:27 -0300

    multiple simulation for stress testing

--- tournament/simulation.py
+++ tournament/simulation.py
@@ -1,15 +1,18 @@
-
+from time import time
 from tournament.swiss_tournament_simulator import SwissTournamentSimulator
 from utils.player import Player
+start = time()
+for _ in range(200):
+    for j in [5, 7, 9, 17, 33]:
+        players = []
+        for i in range(j):
+            players.append(
+                Player(
+                    name=f"Player{i}",
+                    strategy=None
+                )
+            )
 
-players = []
-for i in range(11):
-    players.append(
-        Player(
-            name=f"Player{i}",
-            strategy=None
-        )
-    )
-
-tournament = SwissTournamentSimulator(players)
-tournament.run()
+        tournament = SwissTournamentSimulator(players)
+        tournament.run()
+print(time()-start, " Segundos")
